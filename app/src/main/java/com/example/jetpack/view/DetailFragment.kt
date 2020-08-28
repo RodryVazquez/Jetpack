d4e0a5b0d5383c12b4647dd2b88e5846476ddeab
rodrigovazquez <development.360@hotmail.com>
2020-08-28 15:03:10 -0600

    integrate basic navigation between fragments and create icons for float buttons

--- app/src/main/java/com/example/jetpack/view/DetailFragment.kt
+++ app/src/main/java/com/example/jetpack/view/DetailFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.jetpack.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class DetailFragment : Fragment() {
 
@@ -15,5 +18,13 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnListFragment.setOnClickListener {
+            val action = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
